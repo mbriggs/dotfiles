dca2b91804dec5e785f4a771dbe368249e745f67
Matt Briggs <matt@mattbriggs.net>
2018-08-09 07:42:26 -0400

    Quote and bash alias added

--- config/aliases.sh
+++ config/aliases.sh
@@ -8,7 +8,9 @@ alias gd="git diff"
 alias gap='git add -N . && git add -p'
 alias gdc="git diff --cached"
 alias gca="read msg; git commit -am \"$msg\""
+alias ga="git add -A ."
 alias g="git"
 alias gs="git status"
 alias less="less -R"
+
 alias copy-settings="for example in settings/*.example; do cp -v \$example settings/\$(basename \$example .example); done"
